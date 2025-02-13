f7a9415be680f043244f93933399eba5876e6265
ranaroy-ai <143075343+ranaroy-ai@users.noreply.github.com>
2025-02-13 19:34:37 +0530

    Update score.py

--- score.py
+++ score.py
@@ -3,13 +3,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "FastAPI running on Azure ML"}
 
 @app.post("/predict")
 def predict(data: dict):
     return {"prediction": "some result"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5001)
