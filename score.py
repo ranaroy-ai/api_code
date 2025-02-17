4cfafddd99b183df7aafe1d2aa65165e4c8164c5
ranaroy-ai <143075343+ranaroy-ai@users.noreply.github.com>
2025-02-17 09:22:05 +0530

    Update score.py

--- score.py
+++ score.py
@@ -1,12 +1,11 @@
-import uvicorn
+import logging
 from fastapi import FastAPI
 
 app = FastAPI()
 
+def init():
+    logging.info("Service initialized.")
 
-@app.post("/predict")
-def predict(data: dict):
-    return {"prediction": "some result"}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5001)
+@app.get("/")
+def run():
+    return {"message": "Hello from FastAPI!"}
